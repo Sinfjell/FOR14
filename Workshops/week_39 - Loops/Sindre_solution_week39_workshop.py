111afd02b4093e21e2cc129f000b6772275a2d00
Sinfjell <52018161+Sinfjell@users.noreply.github.com>
2021-10-03 09:34:41 +0200

    Update Sindre_solution_week39_workshop.py
    dsf

--- Workshops/week_39 - Loops/Sindre_solution_week39_workshop.py
+++ Workshops/week_39 - Loops/Sindre_solution_week39_workshop.py
@@ -27,25 +27,73 @@ moreDraws = input('Do you wish to draw a random number? Type "Y" to continue, an
 while moreDraws == 'Y':
     
     # get bounds from user
-    lower = int(input('Please enter a lower bound: '))
-    upper = int(input('Please enter an upper bound: '))
-
-    if lower != int: 
-        print("Your lower input is  not an integer.")
-        lower = int(input('Please enter a lower bound: '))
-
-    elif upper != int: 
-        print("Your upper input is  not an integer.")
-        upper = int(input('Please enter a upper bound: '))
-        
-    else: 
-        # draw random number
-        rand_num = random.randint(lower, upper)
-        print('\nYour random draw is... {}!'.format(rand_num))
-
-    # ask whether to continue 
-    # (notice that if the user types anything but "y", the loop will terminate)
-    moreDraws = input('Do you wish another draw? Type "Y" to continue, and "N" to quit: ').upper()
+    lower = input('Please enter a lower bound: ')
+    upper = input('Please enter an upper bound: ')
     
+    # check that the user has entered digits only
+    if lower.isdigit() and upper.isdigit():
+        
+        # convert inputs to int
+        lower = int(lower)
+        upper = int(upper)
+
+        
+        # check that the user has entered valid bounds
+        if lower > upper:
+            print('\nInvalid inputs!')
+            print('You must enter a lower bound that is lower than the upper bound.')
+        
+        else:
+            # draw random number
+            rand_num = random.randint(lower, upper)
+            print('\nYou asked for a random number between {} and {}.'.format(lower, upper))
+            print('Your random draw is... {}!'.format(rand_num))
+            
+            rand_num = random.randint(lower, upper)
+            print('\nYour random draw is... {}!'.format(rand_num))
+            
+            # ask whether to continue 
+            # (notice that if the user types anything but "y", the loop will terminate)
+            moreDraws = input('Do you wish another draw? Type "Y" to continue, and "N" to quit: ').upper()
+            
+    else:
+        print('\nInvalid inputs!')
+        print('You can only enter whole numbers.')
+  
 print('\nThank you for using the random number generator!')
+
+#%% 
+# Exercise 1a
+
+word = input("Write out a english word: ")
+count = 0
+ 
+for vowel in word :
+    if vowel in ("a", "e", "i", "o", "u"):
+        count = count + 1
+
+    
+print("In your selected word, there are {} vowels".format(count))
+
+
+
+#%%
+
+Input = word
+Output = Count of vowels
+
+for all lettters in word do :
+    if word = aecdu:
+        count = count + 1
+        
+
+
+
+
+
+
+
+
+
+
     
